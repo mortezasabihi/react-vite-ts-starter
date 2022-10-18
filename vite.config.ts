e65315b300cc30b1877ced537cb6f72305012432
Morteza Sabihi <mortezasabihi87@gmail.com>
2022-10-18 20:53:30 +0330

    chore: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,24 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
